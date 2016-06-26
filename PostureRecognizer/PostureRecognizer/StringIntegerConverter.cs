f7e46aca63e28632faed43cf8bf614a810db376f
huiya9527 <huiya9527@163.com>
2016-06-26 21:29:33 +0800

    基本完成
    基本完成， 可以配合定义工具完成识别过程

---
+++ PostureRecognizer/PostureRecognizer/StringIntegerConverter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostureRecognizer
+{
+    class StringIntegerConverter
+    {
+        public static string convertToString(int number)
+        {
+            if (number >= 0 && number <= 9)
+            {
+                return number.ToString();
+            }
+            else if (number >= 10 && number <= 35)
+            {
+                char temp = (char)(number - 10 + 'A');
+                return temp.ToString();
+            }
+            else if (number >= 36 && number <= 61)
+            {
+                char temp = (char)(number - 36 + 'a');
+                return temp.ToString();
+            }
+            else if (number == 62)
+            {
+                char temp = '!';
+                return temp.ToString();
+            }
+            else if (number == 63)
+            {
+                char temp = '?';
+                return temp.ToString();
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static int convertToInteger(string ss)
+        {
+            char temp = ss.ToCharArray()[0];
+            if (temp >= '0' && temp <= '9')
+            {
+                return (int)(temp - '0');
+            }
+            else if (temp >= 'A' && temp <= 'Z')
+            {
+                return (int)(temp - 'A') + 10;
+            }
+            else if (temp >= 'a' && temp <= 'z')
+            {
+                return (int)(temp - 'a') + 36;
+            }
+            else if (temp == '1')
+            {
+                return 62;
+            }
+            else if (temp == '?')
+            {
+                return 63;
+            }
+            else
+            {
+                return int.MaxValue;
+            }
+        }
+    }
+}
